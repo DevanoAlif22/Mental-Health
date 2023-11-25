d9648b6ab9d1687d334aa6d0eba9b506df026c21
DevanoAlif22 <devanorama123@gmail.com>
2023-11-25 15:22:54 +0700

    login2

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -15,52 +15,67 @@
                 <div class="col-6"  style="background-color: #0A6EF6; height: 100vh;">
                     <div class="container">
                         <img class="logo" src="{{ asset('images/login/logo.png')}}" alt="">
-                        <div id="carouselExampleCaptions" class="carousel slide">
+                        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-indicators">
-                              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                             </div>
                             <div class="carousel-inner">
-                              <div class="carousel-item active d-flex justify-content-center">
-                                <img src="{{asset("/images/login/slide1.png")}}" class="d-block w-50 mb-4" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                  <h5>First slide label</h5>
-                                  <p>Some representative placeholder content for the first slide.</p>
+                                <div class="carousel-item active text-center">
+                                    <img src="{{asset("/images/login/slide1.png")}}" class="img-caraousel d-block mx-auto w-50" alt="...">
+                                    <div style="text-align: left" class="carousel-caption d-none d-md-block">
+                                        <h5>First slide label</h5>
+                                        <p>Some representative placeholder content for the first slide.</p>
+                                    </div>
                                 </div>
-                              </div>
-                              <div class="carousel-item">
-                                <img src="..." class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                  <h5>Second slide label</h5>
-                                  <p>Some representative placeholder content for the second slide.</p>
+                                <div class="carousel-item text-center">
+                                    <img src="{{asset("/images/login/slide2.png")}}" class="d-block mx-auto w-50 img-caraousel" alt="...">
+                                    <div style="text-align: left" class="carousel-caption d-none d-md-block">
+                                        <h5>Second slide label</h5>
+                                        <p>Some representative placeholder content for the second slide.</p>
+                                    </div>
                                 </div>
-                              </div>
-                              <div class="carousel-item">
-                                <img src="..." class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                  <h5>Third slide label</h5>
-                                  <p>Some representative placeholder content for the third slide.</p>
+                                <div class="carousel-item text-center">
+                                    <img src="{{asset("/images/login/slide3.png")}}" class="d-block mx-auto w-50 img-caraousel" alt="...">
+                                    <div style="text-align: left" class="carousel-caption d-none d-md-block">
+                                        <h5>Third slide label</h5>
+                                        <p>Some representative placeholder content for the third slide.</p>
+                                    </div>
                                 </div>
-                              </div>
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Next</span>
-                            </button>
-                          </div>
+                        </div>
+
+                        <div class="container" style="color: white; ">
+                            <h2 style="padding-left: 15px;">Selamat datang!</h2>
+                            <p style="padding-left: 15px;">Di website Mental health, Pulihkan kesehatan mental mu disini!</p>
+
+                        </div>
+
+
                     </div>
                 </div>
-                <div class="col-6">
-
+                <div class="col-6" style="display: flex;">
+                    <div class="form-login" style="margin: auto">
+                        <h3>Masuk</h3>
+                    </div>
                 </div>
             </div>
     </div>
 
+
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            // Tambahkan script ini untuk membuat carousel bergerak secara otomatis
+            var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleCaptions'), {
+                interval: 3000, // Ganti dengan interval yang diinginkan dalam milidetik (contohnya 3000 untuk 3 detik)
+                wrap: true, // Untuk membuat carousel bergerak secara infinity
+                pause: 'hover' // Menghentikan gerakan carousel saat kursor di atasnya
+            });
+        });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
